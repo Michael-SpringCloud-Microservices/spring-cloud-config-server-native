59acea93f887cd70baa1101e8c01a95784cfc997
MICHAEL PHILOMI <MichaelPhilomi.PaneerSelvam@merrillcorp.com>
2019-02-25 15:44:34 +0530

    Adding the first set of native(local) file based configuration

---
+++ src/test/java/com/in28minutes/microservices/springcloudconfigservernative/SpringCloudConfigServerNativeApplicationTests.java
@@ -0,0 +1,17 @@
+package com.in28minutes.microservices.springcloudconfigservernative;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class SpringCloudConfigServerNativeApplicationTests {
+
+	//@Test
+	public void contextLoads() {
+	}
+
+}
